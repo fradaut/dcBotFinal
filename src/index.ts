cc39be0dece32d02857785f81d774052a29fdd1d
NFalone <qoo19830526@gmail.com>
2023-07-08 23:48:58 +0800

    chore: 導入 驗證 和 連線 1. 驗證env檔案有確實設置 2. 連線資料庫

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import { Client } from 'discord.js';
 import dotenv from 'dotenv';
 
 import IntentOptions from './config/IntentOptions.js';
+import connectDatabase from './database/connectDatabase.js';
+import validateEnv from './utils/validateEnv.js';
 
 dotenv.config();
 
 (async () => {
+  if (!validateEnv()) return;
+
   const BOT = new Client({ intents: IntentOptions });
 
+  await connectDatabase();
   await BOT.login(process.env.TOKEN);
 })();
