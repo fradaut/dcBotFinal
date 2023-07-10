9a2fc62acf8833808f31c7e188ed5a5fbddc6317
NFalone <qoo19830526@gmail.com>
2023-07-11 00:45:48 +0800

    feat: 註冊命令, 讀取命令 1. 註冊全域斜線命令 2. 讀取commands資料夾底下所有命令

--- src/core/loader.ts
+++ src/core/loader.ts
@@ -1,13 +1,36 @@
-// import { REST } from 'discord.js';
-import { Client } from 'discord.js';
+import {
+  REST, Client, SlashCommandBuilder, Routes, CommandInteraction,
+} from 'discord.js';
 import fg from 'fast-glob';
 import path from 'path';
+import { Command } from '../interfaces/Command.js';
+import CommandList from '../commands/_CommandList.js';
 
-// const updateSlashCommands = async (commands) => {
-//   const rest = new REST({ version: '10' }).setToken('token');
+const updateSlashCommands = async (commands: SlashCommandBuilder[]) => {
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN as string);
 
-//   await rest.put();
-// };
+  await rest.put(
+    Routes.applicationCommands(
+      process.env.CLIENT_ID as string,
+    ),
+    {
+      body: commands,
+    },
+  );
+};
+
+const loadCommands = async () => {
+  const commands: SlashCommandBuilder[] = [];
+  const files = await fg('./src/commands/**/index.ts');
+
+  await Promise.all(files.map(async (file) => {
+    const cmd: Command = await import(path.resolve(file));
+    commands.push(cmd.command);
+    CommandList.set(cmd.command.name, cmd.run);
+  }));
+
+  await updateSlashCommands(commands);
+};
 
 const loadEvents = async (client: Client) => {
   const files = await fg('./src/events/**/index.ts');
@@ -16,7 +39,9 @@ const loadEvents = async (client: Client) => {
     const eventFile = await import(path.resolve(file));
 
     const { name, isOnce, hasClient } = eventFile.event;
-    const action = hasClient ? () => eventFile.action(client) : () => eventFile.action();
+    const action = hasClient
+      ? (interaction: CommandInteraction) => eventFile.action(client, interaction)
+      : (interaction: CommandInteraction) => eventFile.action(interaction);
 
     if (isOnce) {
       client.once(name, action);
@@ -27,7 +52,7 @@ const loadEvents = async (client: Client) => {
 };
 
 const loader = async (client: Client) => {
-  // await loadCommands(client);
+  await loadCommands();
   await loadEvents(client);
 };
 
