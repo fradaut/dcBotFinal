1c4df22ead2f304cacf840c53125e63db2ef7cb4
NFalone <qoo19830526@gmail.com>
2023-07-16 18:25:05 +0800

    feat: 命令自動完成的map 儲存自動完成命令所對應的map

---
+++ src/events/onInteraction/autocompleteData.ts
@@ -0,0 +1,14 @@
+const autocompleteData = new Map([
+  ['type', ['database']],
+  ['database', ['channelWelcomeID',
+    'channelLeaveID',
+    'channelMemberOnlineID',
+    'channelMemberCountID',
+    'channelMessageDeleteID',
+    'channelVoiceCreateID',
+    'categoryVoiceCreateID',
+
+    'roleAdminID']],
+]);
+
+export default autocompleteData;
