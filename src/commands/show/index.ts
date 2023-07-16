15507e59e562568bc4969eb2abe5d5bbb810b520
NFalone <qoo19830526@gmail.com>
2023-07-17 00:06:20 +0800

    feat: 顯示資料庫內的資料 1.將所有ID設定顯示為標記和名稱. 2.找不到該ID匹配的名稱將直接輸出ID.

---
+++ src/commands/show/index.ts
@@ -0,0 +1,54 @@
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import GuildChannel from '../../database/models/GuildChannel.js';
+import GuildRole from '../../database/models/GuildRole.js';
+
+const getChannel = (interaction: CommandInteraction, channelID: string) => {
+  const channel = interaction.guild!.channels.cache.get(channelID);
+  return channel ? `<#${channel.id}>` : channelID;
+};
+const getCategory = (interaction: CommandInteraction, categoryID: string) => {
+  const category = interaction.guild!.channels.cache.get(categoryID);
+  return category ? `${category.name}` : categoryID;
+};
+const getRole = (interaction: CommandInteraction, roleID: string) => {
+  const role = interaction.guild!.roles.cache.get(roleID);
+  return role ? `<@&${role.id}>` : roleID;
+};
+
+export const command = new SlashCommandBuilder()
+  .setName('show')
+  .setDescription('顯示資料庫資訊');
+
+export const run = async (interaction: CommandInteraction) => {
+  await interaction.deferReply({ ephemeral: true });
+
+  if (!interaction.inGuild()) {
+    await interaction.followUp('此命令無法在私訊使用');
+    return;
+  }
+
+  try {
+    const guildChannelDoc = await GuildChannel.findOne({ guildID: interaction.guildId });
+    const guildRoleDoc = await GuildRole.findOne({ guildID: interaction.guildId });
+
+    if (!guildChannelDoc || !guildRoleDoc) {
+      interaction.followUp({ content: '此伺服器尚未初始化, 請使用 bot 指令進行設定', ephemeral: true });
+    }
+
+    interaction.followUp({
+      content: `伺服器ID: ${guildChannelDoc!.guildID}
+歡迎頻道: ${getChannel(interaction, guildChannelDoc!.channelWelcomeID)}
+離開頻道: ${getChannel(interaction, guildChannelDoc!.channelLeaveID)}
+線上成員頻道: ${getChannel(interaction, guildChannelDoc!.channelMemberOnlineID)}
+成員總數頻道: ${getChannel(interaction, guildChannelDoc!.channelMemberCountID)}
+被刪除訊息頻道: ${getChannel(interaction, guildChannelDoc!.channelMessageDeleteID)}
+創建語音頻道頻道: ${getChannel(interaction, guildChannelDoc!.channelVoiceCreateID)}
+創建語音頻道類別: ${getCategory(interaction, guildChannelDoc!.categoryVoiceCreateID)}
+管理員身分組: ${getRole(interaction, guildRoleDoc!.roleAdminID)}`,
+      ephemeral: true,
+    });
+  } catch (error) {
+    console.error(error);
+    interaction.followUp({ content: '發生錯誤, 請聯絡開發者', ephemeral: true });
+  }
+};
