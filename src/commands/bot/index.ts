d28cd55e3b1d02e4cb01a823c8ef21118db170e0
NFalone <qoo19830526@gmail.com>
2023-07-16 18:24:18 +0800

    feat: 對資料庫進行更改的命令 可以對資料庫內的值進行更改.

---
+++ src/commands/bot/index.ts
@@ -0,0 +1,47 @@
+import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import autocompleteData from '../../events/onInteraction/autocompleteData.js';
+import modelMap from '../../database/modelMap.js';
+
+export const command = new SlashCommandBuilder()
+  .setName('bot')
+  .setDescription('機器人相關設定')
+  .addStringOption((option) => option.setName('type')
+    .setDescription('選擇需要進行設定的類別')
+    .setRequired(true)
+    .setAutocomplete(true))
+  .addStringOption((option) => option.setName('key')
+    .setDescription('選擇需要更改的key')
+    .setRequired(true)
+    .setAutocomplete(true))
+  .addStringOption((option) => option.setName('value')
+    .setDescription('輸入新的值')
+    .setRequired(true));
+
+export const run = async (interaction: CommandInteraction) => {
+  await interaction.deferReply({ ephemeral: true });
+
+  const key = interaction.options.get('key')!.value as string;
+  const { value } = (interaction.options.get('value')!);
+
+  if (!autocompleteData.get('database')!.includes(key)) {
+    interaction.followUp({ content: `輸入的 ${key} 不存在`, ephemeral: true });
+    return;
+  }
+
+  const model = modelMap.get(key)!;
+
+  model.findOneAndUpdate(
+    { guildID: interaction.guildId },
+    { [key]: value },
+    { new: true, useFindAndModify: false },
+  ).then((doc) => {
+    if (doc) {
+      interaction.followUp({ content: `已將 ${key} 更改為 ${value}`, ephemeral: true });
+    } else {
+      interaction.followUp({ content: '此伺服器尚未初始化, 請使用 initial 指令進行設定', ephemeral: true });
+    }
+  }).catch((error) => {
+    console.error(error);
+    interaction.followUp({ content: '發生錯誤', ephemeral: true });
+  });
+};
