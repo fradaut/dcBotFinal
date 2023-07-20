e077b49c28b417aafbc9cd9a2df1d414ecea21b4
NFalone <qoo19830526@gmail.com>
2023-07-21 01:00:59 +0800

    feat: 導入partial配置檔案 partial透過獨立檔案進行配置.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ import IntentOptions from './config/IntentOptions.js';
 import connectDatabase from './database/connectDatabase.js';
 import validateEnv from './utils/validateEnv.js';
 import loader from './core/loader.js';
+import PartialOptions from './config/PartialOptions.js';
 
 dotenv.config();
 
 (async () => {
   if (!validateEnv()) return;
 
-  const BOT = new Client({ intents: IntentOptions });
+  const BOT = new Client({ partials: PartialOptions, intents: IntentOptions });
 
   await loader(BOT);
 
