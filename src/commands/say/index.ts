3aaab407ad67bb7849ed7dc06b10c69dab892932
NFalone <qoo19830526@gmail.com>
2023-07-22 22:25:01 +0800

    feat: 指定身分組發送訊息
    現在給予身分組ID也可發送訊息.
    
    獲取頻道的方式獨立為一個函式並改為獲取一個頻道列表.

--- src/commands/say/index.ts
+++ src/commands/say/index.ts
@@ -2,6 +2,8 @@ import {
   CommandInteraction, DMChannel, NewsChannel, SlashCommandBuilder, TextChannel, ThreadChannel,
 } from 'discord.js';
 import checkGuild from '../../utils/checkGuild.js';
+import getChannels from './getChannels.js';
+import validateRole from '../../utils/validateRole.js';
 
 export const command = new SlashCommandBuilder()
   .setName('say')
@@ -17,36 +19,43 @@ export const run = async (interaction: CommandInteraction) => {
   await interaction.deferReply({ ephemeral: true });
 
   if (!checkGuild(interaction)) return;
+  if (!await validateRole(interaction)) return;
 
   const messageid = interaction.options.get('messageid')!.value as string;
   const targetid = interaction.options.get('targetid')!.value as string;
 
   const message = await interaction.channel!.messages.fetch(messageid);
-
   if (!message) {
     interaction.followUp('指定的訊息不存在');
+    return;
   }
 
-  interaction.guild!.channels.fetch(targetid)
-    .catch(() => interaction.guild!.members.fetch(targetid).then((user) => user.createDM()))
-    .then(async (channel) => {
-      if (!channel) {
-        interaction.followUp('指定的頻道或使用者不存在');
-        return;
-      }
+  const channels = await getChannels(targetid, interaction);
+  if (channels.length === 0) {
+    await interaction.followUp('指定的頻道或使用者不存在');
+    return;
+  }
 
+  try {
+    await Promise.all(channels.map(async (channel) => {
       if (channel instanceof TextChannel
-        || channel instanceof DMChannel
-        || channel instanceof NewsChannel
-        || channel instanceof ThreadChannel) {
-        channel.send(message.content).then(() => interaction.followUp('發送成功'));
-        return;
+      || channel instanceof DMChannel
+      || channel instanceof NewsChannel
+      || channel instanceof ThreadChannel) {
+        try {
+          await channel.send(message.content);
+        } catch (error) {
+          await interaction.followUp('訊息發送錯誤');
+          console.error(error);
+          throw error;
+        }
+      } else {
+        await interaction.followUp('指定的頻道不是文字頻道');
       }
+    }));
 
-      interaction.followUp('指定的頻道不是文字頻道');
-    })
-    .catch((error) => {
-      console.error(error.message);
-      interaction.followUp('發生錯誤，請稍後再試');
-    });
+    await interaction.followUp('發送成功');
+  } catch (error) {
+    console.error('迴圈已中斷', error);
+  }
 };
