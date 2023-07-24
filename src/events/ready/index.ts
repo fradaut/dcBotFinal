c3223ab17e72207e4b5d8a0c2b5425b1d4cf3327
NFalone <qoo19830526@gmail.com>
2023-07-24 21:25:21 +0800

    feat: 導入線上人數更新

--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,4 +1,5 @@
 import { Client, ClientUser, Events } from 'discord.js';
+import updateOnlineCount from './updateOnlineCount.js';
 
 export const event = {
   name: Events.ClientReady,
@@ -8,4 +9,5 @@ export const event = {
 
 export const action = async (client: Client) => {
   console.log(`Ready! Logged in as ${(client.user as ClientUser).tag}`);
+  setInterval(() => { updateOnlineCount(client); }, 60000);
 };
